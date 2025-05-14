4c1dea58a1e255f3bbec6b24da268eb019aaebe5
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-05-14 17:58:43 +0530

    ss

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1-dev.gvndevops12.tech, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e "component_name=$1" roboshop.yml
+ansible-playbook -i $1-{{ env }}.gvndevops12.tech, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e "component_name=$1" roboshop.yml
